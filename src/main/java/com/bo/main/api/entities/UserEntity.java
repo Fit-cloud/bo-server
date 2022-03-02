e3acb33c8a97093e6fb59f0abdb0947b5b28987b
[상품개발팀]서민규 <minkyu.seo@lotte.net>
2022-03-02 16:07:11 +0900

    Add : JPA Config

---
+++ src/main/java/com/bo/main/api/entities/UserEntity.java
@@ -0,0 +1,28 @@
+package com.bo.main.api.entities;
+
+
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@RequiredArgsConstructor
+@Entity
+@Table(name = "USER")
+public class UserEntity implements Serializable {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
